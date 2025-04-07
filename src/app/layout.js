a5c2fff707ff48d00c4e615774208fd4090a1163
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-04-07 15:47:34 +0300

    basic project styles
    Додано базові кольори, вимоги до типографії, кілька кастомних класів, налаштування для темної та світлої теми.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 
+import { ThemeProvider } from 'next-themes';
 import { montserrat } from './fonts';
 
 // let str3 = 'Hallo';
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased `}>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
