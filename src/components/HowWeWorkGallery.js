b3427dbcbd896c6d638a47c851f2dd4038b5b50b
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-04-16 18:28:51 +0300

    update of pictures, adaptation to devices, elaboration of styles on the title

--- src/components/HowWeWorkGallery.js
+++ src/components/HowWeWorkGallery.js
@@ -1,59 +1,66 @@
 'use client';
+import PropTypes from 'prop-types';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useTheme } from 'next-themes';
 import Title from './Title';
 
-const HowWeWorkGallery = () => {
-  const [currentItem, setCurrentItem] = useState(1);
+const HowWeWorkGallery = ({ items }) => {
+  const { theme } = useTheme();
+  const [currentItem, setCurrentItem] = useState(0);
   const [visible, setVisible] = useState(true);
+  const containerRef = useRef(null);
+  const isScrolling = useRef(false);
 
-  const ITEMS = [
-    {
-      id: '835sjHPnt6E',
-      title: 'Research and Design',
-      text: 'At KiWiCode Solutions, our journey begins with in-depth research and design. We conduct thorough market analysis and gather insights into your project`s goals and target audience. Our expert designers craft visually appealing and user-centric designs that set the foundation for a successful project.',
-      img: '/images/how_we_work_1.png',
-    },
-    {
-      id: 'e96fe2AG6Kb',
-      title: 'Development',
-      text: 'Once the design is approved, our skilled development team steps in. Using cutting-edge technologies, we transform designs into functional tech products. At KiWiCode Solutions, we specialize in creating custom solutions tailored to your specific needs, ensuring your project is unique and fully responsive.',
-      img: '/images/how_we_work_2.png',
-    },
-    {
-      id: 'v6k2Cj4b6VN',
-      title: 'Testing and Optimization',
-      text: 'Quality is our priority. In the testing and optimization phase, we perform extensive quality assurance to identify and resolve any issues, ensuring a seamless user experience. We also optimize performance, ensuring your project loads quickly and efficiently.',
-      img: '/images/how_we_work_3.png',
-    },
-    {
-      id: 'r5uhG9CLx25',
-      title: 'Launching the Project',
-      text: 'KiWiCode Solutions ensures a smooth and secure project launch. We handle the technical aspects of deployment, making your project accessible to your audience. This is the exciting moment when your project goes live and becomes available to users.',
-      img: '/images/how_we_work_4.png',
-    },
-  ];
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
 
-  const handleItemHover = (id) => {
-    if (id === currentItem) return;
-    setVisible(false);
-    setTimeout(() => {
-      setCurrentItem(id);
-      setVisible(true);
-    }, 200);
-  };
+    // Додамо перевірку на ширину екрану
+    const isDesktop = window.innerWidth >= 1280;
+    if (!isDesktop) return;
+
+    const handleScroll = (e) => {
+      e.preventDefault();
+      if (isScrolling.current) return;
+
+      isScrolling.current = true;
+
+      if (e.deltaY > 0) {
+        setCurrentItem((prev) => Math.min(prev + 1, items.length - 1));
+      } else {
+        setCurrentItem((prev) => Math.max(prev - 1, 0));
+      }
+
+      setVisible(false);
+      setTimeout(() => {
+        setVisible(true);
+        isScrolling.current = false;
+      }, 500);
+    };
+
+    container.addEventListener('wheel', handleScroll, { passive: false });
+
+    return () => {
+      container.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="flex items-center ml-[47px] gap-x-[74px] justify-between relative z-[1]">
-      <div className="w-[1px] h-[440px] rounded-[1px] absolute top-1/2 -translate-y-1/2 -left-[42px] bg-[#E6E9EA]" />
-      <ul className="w-[314px] h-full flex flex-col gap-y-[25px]">
-        {ITEMS.map((el, id) => (
+    <div
+      ref={containerRef}
+      className="flex items-center xl:ml-[47px] xl:gap-x-[74px] justify-between relative z-[1]"
+    >
+      <div className="hidden w-[1px] h-[440px] rounded-[1px] xl:absolute top-1/2 -translate-y-1/2 -left-[42px] bg-[#E6E9EA]" />
+
+      <ul className="hidden w-[314px] h-full xl:flex flex-col gap-y-[25px]">
+        {items.map((el, index) => (
           <li
             key={el.id}
-            onMouseEnter={() => handleItemHover(id)}
-            className={`${currentItem !== id ? 'opacity-50' : 'opacity-100'} relative transition-opacity duration-300`}
+            // onMouseEnter={() => handleItemHover(index)}
+            className={`${currentItem !== index ? 'opacity-50' : 'opacity-100'} relative transition-opacity duration-300`}
           >
-            {currentItem === id && (
+            {currentItem === index && (
               <div className="absolute top-1/2 -translate-y-1/2 -left-[44px] w-[5px] h-20 rounded-[3px] bg-[#5BD187]" />
             )}
 
@@ -64,25 +71,56 @@ const HowWeWorkGallery = () => {
       </ul>
 
       <div
-        className={`
-    w-[630px] h-[461px] flex items-end relative rounded-base overflow-hidden p-10 my-auto
-    transition-opacity duration-2000 ease-in-out
-    ${visible ? 'opacity-100' : 'opacity-0'}
-  `}
+        className={`w- [345px] h-[697px] md:w-[704px] md:h-[524px] xl:w-[630px] xl:h-[461px] flex items-end relative rounded-base overflow-hidden p-6 md:p-10 my-auto transition-opacity duration-2000 ease-in-out`}
       >
         <Image
-          src={ITEMS[currentItem].img}
-          width={360}
-          height={461}
+          src={items[currentItem].img}
+          width={1000}
+          height={1000}
           className="w-full h-full object-center object-cover absolute top-0 left-0 transition-opacity duration-2000 ease-in-out"
           alt=""
         />
-        <div className="bg-white rounded-base p-6 relative z-[1] mt-auto transition-opacity duration-2000 ease-in-out">
-          <p>{ITEMS[currentItem].text}</p>
+        <div className="w-full h-full absolute top-0 left-0 hww-gallery-gradient z-[2]" />
+        {items[currentItem].component && currentItem === 0 && (
+          <Image
+            src={items[currentItem].component}
+            width={600}
+            height={700}
+            className="w-[400px] h-[402px] absolute top-[-10px] xl:top-0 left-1/2 transform -translate-x-1/2 transition-opacity duration-2000 ease-in-out"
+            alt=""
+          />
+        )}
+        {items[currentItem].component && currentItem === 3 && (
+          <Image
+            src={items[currentItem].component}
+            width={326}
+            height={307}
+            className="absolute top-[-48px] right-[-38px] transition-opacity duration-2000 ease-in-out"
+            alt=""
+          />
+        )}
+        {items[currentItem].component && (currentItem === 0 || currentItem === 3) && (
+          <div className="w-full h-[55px] rounded-full absolute top-1/2 left-0 -translate-y-1/2 bg-[#68F2FF] blur-[166px] z-[4]" />
+        )}
+        <div
+          className={`rounded-base p-6 relative z-[5] mt-auto transition-opacity duration-2000 ease-in-out ${theme === 'dark' ? 'bg-bgColor text-white' : 'bg-white text-main'}`}
+        >
+          <Title styles={'xl:hidden font-semibold mb-2'}>{items[currentItem].title}</Title>
+          <p>{items[currentItem].text}</p>
         </div>
       </div>
     </div>
   );
+};
+
+HowWeWorkGallery.propTypes = {
+  items: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    component: PropTypes.string,
+  }),
 };
 
 export default HowWeWorkGallery;
--- src/components/sections/HowWeWork.js
+++ src/components/sections/HowWeWork.js
@@ -1,12 +1,50 @@
 import HowWeWorkGallery from '../HowWeWorkGallery';
 import Title from '../Title';
+// import { useTranslations } from 'next-intl';
 
 const HowWeWorkSection = () => {
+  // const t = useTranslations('HowWeWorks');
+  const PRINCIPLES = [
+    {
+      id: '835sjHPnt6E',
+      title: 'Research and Design',
+      // title: t('principles.0.title'),
+      // text: t('principles.0.text'),
+      text: 'At KiWiCode Solutions, our journey begins with in-depth research and design. We conduct thorough market analysis and gather insights into your project`s goals and target audience. Our expert designers craft visually appealing and user-centric designs that set the foundation for a successful project.',
+      img: '/images/how_we_work_1.png',
+      component: '/images/how_we_work_1_component.png',
+    },
+    {
+      id: 'e96fe2AG6Kb',
+      title: 'Development',
+      // title: t('principles.1.title'),
+      // text: t('principles.1.text'),
+      text: 'Once the design is approved, our skilled development team steps in. Using cutting-edge technologies, we transform designs into functional tech products. At KiWiCode Solutions, we specialize in creating custom solutions tailored to your specific needs, ensuring your project is unique and fully responsive.',
+      img: '/images/how_we_work_2.jpg',
+    },
+    {
+      id: 'v6k2Cj4b6VN',
+      title: 'Testing and Optimization',
+      // title: t('principles.2.title'),
+      // text: t('principles.2.text'),
+      text: 'Quality is our priority. In the testing and optimization phase, we perform extensive quality assurance to identify and resolve any issues, ensuring a seamless user experience. We also optimize performance, ensuring your project loads quickly and efficiently.',
+      img: '/images/how_we_work_3.jpg',
+    },
+    {
+      id: 'r5uhG9CLx25',
+      title: 'Launching the Project',
+      // title: t('principles.3.title'),
+      // text: t('principles.3.text'),
+      text: 'KiWiCode Solutions ensures a smooth and secure project launch. We handle the technical aspects of deployment, making your project accessible to your audience. This is the exciting moment when your project goes live and becomes available to users.',
+      img: '/images/how_we_work_4.png',
+      component: '/images/how_we_work_4_component.png',
+    },
+  ];
   return (
     <section className="w-full relative">
-      <div className="w-[1066px] mx-auto ">
+      <div className="w-full md:px-8 xl:px-0 xl:w-[1066px] mx-auto">
         <Title tag={'h2'}>How We Work</Title>
-        <HowWeWorkGallery />
+        <HowWeWorkGallery items={PRINCIPLES} />
       </div>
       <div className="absolute -left-[311px] top-[397px] w-[604px] h-[604px] rounded-full hww-section-gradient blur-[1000px] z-0" />
     </section>
