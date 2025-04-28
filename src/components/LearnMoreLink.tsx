eb987d15e1cd0acf2982f2c28e5415bd1fe833a1
Sanchos111 <kholeksandr.kiwicode@gmail.com>
2025-04-28 13:05:20 +0200

    add focus outline in LearnMoreLink

--- src/components/LearnMoreLink.tsx
+++ src/components/LearnMoreLink.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import clsx from 'clsx';
 import { Link } from '@/i18n/navigation';
 import { Arrow } from '@/assets/icons/icons';
@@ -11,28 +10,24 @@ type LearnMoreLinkProps = {
 };
 
 export default function LearnMoreLink({ locale, href, label }: LearnMoreLinkProps) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [hovered, setHovered] = useState<boolean>(false);
-
   return (
     <Link
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
       href={href}
       locale={locale}
       className={clsx(
-        'relative group flex items-center h-12 pl-16 pr-4 overflow-hidden rounded-full transition-colors duration-400',
-        'bg-transparent text-dark',
-        locale === 'ua' ? 'w-[250px]' : 'w-[207px]'
+        'relative group flex items-center min-w-38 h-12 pl-14 pr-11 overflow-hidden rounded-full transition-colors duration-400',
+        'text-dark outline-[2px] outline-offset-[2px] outline-transparent focus:outline-blue'
+
+        // locale === 'ua' ? 'w-[250px]' : 'w-[207px]'
       )}
     >
-      <span className="absolute left-0 top-0 h-full w-12 bg-accent rounded-full transition-all duration-500 group-hover:w-full z-0" />
+      <span className="absolute left-0 top-0 h-full w-12 bg-accent rounded-full transition-all duration-500 group-hover:w-full" />
 
-      <span className="absolute left-0 top-0 w-12 h-12 flex items-center justify-center z-10">
+      <span className="absolute left-0 top-0 w-12 h-12 flex items-center justify-center">
         <Arrow s />
       </span>
 
-      <span className="relative z-10 transition-colors duration-300 group-hover:text-white font-semibold leading-[1.2]">
+      <span className="relative transition-colors duration-300 group-hover:text-white font-semibold leading-[1.2]">
         {label}
       </span>
     </Link>
