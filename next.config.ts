cafc1836acb380e9017ff45cb2244e2d9ef166cd
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-09-11 13:26:53 +0300

    upd NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ua',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
