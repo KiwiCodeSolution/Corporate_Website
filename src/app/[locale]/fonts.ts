7e11e2bee4a8c32f5513b7ea2d271753ab5ebd51
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-09-11 12:56:42 +0300

    fix font

--- src/app/[locale]/fonts.ts
+++ src/app/[locale]/fonts.ts
@@ -4,7 +4,7 @@ const montserrat = Montserrat({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin', 'cyrillic'],
   display: 'swap',
-  variable: true,
+  variable: '--font-montserrat',
 });
 
 export { montserrat };
